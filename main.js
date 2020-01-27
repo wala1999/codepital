ebeed0ea5c958f83ce3d3a621a0eacb2270204a4
wala0107 <56385702+wala0107@users.noreply.github.com>
2020-01-27 18:59:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,134 +1,232 @@
+
 class Patient {
-    //je définis les parametres
-    constructor(name, maladie, argent,traitement) {
-
-
-        this.name = name;
+    constructor(nom, maladie, argent, poche, etatSante) {
+        this.nom = nom;
         this.maladie = maladie;
         this.argent = argent;
-        this.traitement = traitement;
-        // this.etatSante = malade;
-        // this.localisatin;
-
+        this.poche = poche;
+        this.etatSante = etatSante;
+        this.lieu = ''
 
     }
-
-    goTo(endroit) {
-        this.localisation = endroit;
+    traitement() {
+        this.etatSante = 'traitement'
     }
-    paye(personne, montant) {
-        personne.argent += montant;
-        this.argent -= montant;
+    goTo(lieu) {
+        this.lieu = lieu.nom
     }
+    takeCare() {
 
+    }
+    paye(prix) {
+        this.argent -= prix;
+    }
 
 }
 
-let Marcus=new Patient('mal indenté','100','aucun','malade')
-console.log(Marcus)
-
-let Optimus=new Patient('unsave','200','vide','malade')
-console.log(Optimus)
-
-let Sangoku=new Patient('404','80','vide','malade')
-console.log(Sangoku)
-
-let DarthVader=new Patient('azmatique','110','vide','malade')
-console.log(DarthVader)
-
-let Semicolon=new Patient('syntaxError','60','vide','malade')
-console.log(Semicolon)
-
-////////==================Description du doctor================***///
+let marcus = new Patient('Marcus', 'mal indenté', 100, [], 'malade')
+let optimus = new Patient('Optimus', 'unsave', 200, [], 'malade')
+let Sangoku = new Patient('Sangoku', '404', 80, [], 'malade')
+let darthVader = new Patient('DarthVader', 'azmatique', 110, [], 'malade')
+let semicolon = new Patient('Semicolon', 'syntaxError', 60, [], 'malade')
 
 class Doctor {
-    constructor(nom, argent) {
-        this.nom = nom;
-        this.argent = argent;
-        this.cabinet = [];
-        this.salleDattente = [];
+    constructor(nom, argent, cabinet) {
+        this.nom = nom
+        this.argent = argent
+        this.cabinet = cabinet
+        this.consulation = 50
     }
-
-    
-
-
-    //DIAGNOSTIQUER LES PATIENTS
-    diagnostique(patient) {
-if(patient.maladie == 'mal indenté'){
-    console.log('Le traitement de ${patient.nom} est ${tarif1.traitements)')
-}else if(patient.maladie =="unsave"){
-    console.log('Le traitement de ${patient.nom} est ${traif2.traitement')
-}else if (patient.maladie =="404")  {
-    console.log('Le traitement de ${patient.nom} est ${traif3.traitement')
-}else if (patient.maladie== azmatique){
-    console.log('Le traitement de ${patient.nom} est ${traif4.traitement')
-}
-        
-                console.log("je ne sait pas ce que vous avez");
-
+    diagnostique(maladie) {
+        let traitement
+        switch (maladie) {
+            case 'mal indenté':
+                traitement = `ctrl+maj+f`;
+                break;
+            case 'unsave':
+                traitement = `saveOnFocusChange`;
+                break;
+            case '404':
+                traitement = `CheckLinkRelation`;
+                break;
+            case 'azmatique':
+                traitement = `Ventoline`;
+                break;
+            case 'syntaxError':
+                traitement = `f12+doc`;
+                break;
+            default:
+                console.log("Vous n'etes pas malade !")
         }
-// //propriété etatSante du patient à "diagnostiqué"
-//         this.cabinet.etatSante = "diagnostiqué";
+        return traitement;
 
     }
-
-    // Le doctor fait entré le patient dans son cabinet
-    patientIn(patient) {
-        // le doctor met le patient dans le cabinet
-        this.cabinet.push(patient);
-        // le doctor sort le patient de la salle d'attente
-        this.salleDattente.shift()
+    patientIn(malade) {
+        this.cabinet.push(malade)
     }
-    // le doctor fait sortir le patient de sont cabinet
     patientOut() {
-        this.cabinet.pop();
+        this.argent += this.consulation
+        this.cabinet.pop()
     }
+}
+
+
+let chat ={
+    nom : "chat"
+}
+let doc = new Doctor('Debugger', 0, ['chat'])
+
+class Lieu {
+    constructor(nom) {
+        this.nom = nom
+        this.personnes = []
+    }
+    quitter() {
+        this.personnes.shift()
+    }
+
+
+}
+let sdt = new Lieu("Salle d'attente")
+let allPatients = [marcus, optimus, Sangoku, darthVader, semicolon]
+for (let i = 0; i < allPatients.length; i++) {
+
+    sdt.personnes.push(allPatients[i]);
+}
+
+
+let trait;
+
+
+class Pharmacien {
+    constructor(nom) {
+        this.nom = nom;
+    }
+    prescrire() {
+        let prix
+        switch (trait) {
+            case `ctrl+maj+f`:
+                prix = 60;
+                break;
+            case `saveOnFocusChange`:
+                prix = 100;
+                break;
+            case `CheckLinkRelation`:
+                prix = 35;
+                break;
+            case `Ventoline`:
+                prix = 40
+                break;
+            case `f12+doc`:
+                prix = 20
+                break;
+        }
+        return prix;
+    };
 
 }
 
 
-///LA PHARMACIE
+let pharmacie = new Lieu('Pharmacie')
 
-class Pharmacie {
-    constructor() {
-        this.argent = 0;
-    }
-}
+let pharmacien = new Pharmacien('Le pharmacien')
+let prixMedoc;
 
-(Patient); {
-    let prixMedoc;
-    switch (Patient.traitement) {
-        case "ctrl+maj+f":
-            prixMedoc = 60;
-            break;
-case "saveOnFocuxChange":
-            prixMedoc = 100;
-            break;
-        case "CheckLinkRelation":
-            prixMedoc = 35;
-            break;
-        case "Ventoline":
-            prixMedoc = 40;
-            break;
-        case "f12+doc":
-            prixMedoc = 20;
-            break;
-        default:
-        console.log("Nous somme désolé mais nous n'avons pas de traitement pour vous!");
-    }
+let i = 0;
+let timer = 0
+let diff;
 
+let j = -1
 
-//SI le patient a assez d'argent il paye
+function doSetTimeout(j) {
+    
+        setTimeout(() => {
+        
+            console.log('Le patient est ' + sdt.personnes[0].nom)
+        }, timer += 1000);
+        setTimeout(() => {
+            console.log('Il attend dans la ' + sdt.nom);
+        }, timer += 1000);
+        setTimeout(() => {
+            console.log('Le docteur ' + doc.nom + " le reçoit dans son cabinet.")
+            doc.patientIn(sdt.personnes[0]);
+            sdt.quitter()
+        }, timer += 1000);
+    
+    
+    
+      
+        setTimeout(() => {
+            console.log(allPatients[j].nom + " lui explique son problème");
+    
+        }, timer += 1000);
+    
+        setTimeout(() => {
+            console.log('Le docteur ' + doc.nom + ' lui diagnostique : ' + allPatients[j].maladie);
+    
+        }, timer += 1000);
+    
+        setTimeout(() => {
+            trait = doc.diagnostique(allPatients[j].maladie)
+            console.log('Le docteur lui son indique son traitement qui est : ' + trait + "\nEt lui donne son ordonnace");
+    
+        }, timer += 1000);
+    
+        setTimeout(() => {
+            console.log(allPatients[j].nom + ' remercie le doc et le paye');
+            marcus.paye(doc.consulation)
+            doc.patientOut();
+        }, timer += 1000);
+    
+        setTimeout(() => {
+                console.log(allPatients[j].nom + " se rend a la pharmacie ");
+        marcus.goTo(pharmacie)
+        }, timer += 1000);
+    
+        setTimeout(() => {
+                console.log(allPatients[j].nom + " est arrivé a la pharmacie ");
+    
+        }, timer += 1000);
+    
+        setTimeout(() => {
+                console.log(allPatients[j].nom + " donne l'ordonnace au pharmacien ");
+        prixMedoc = pharmacien.prescrire();
+        }, timer += 1000);
+        
+        setTimeout(() => {
+                console.log(pharmacien.nom + " lui donne son traitement qui coûte " + prixMedoc);
+    
+        }, timer += 1000);
+    
+        setTimeout(() => {
+            console.log(allPatients[j].nom+" paye so medocs");
+            
+            marcus.paye(prixMedoc)
+    
+        }, timer += 1000);
+    
+        setTimeout(() => {
+                console.log(allPatients[j].nom + " prends son medocs");
+    
+        }, timer += 1000);
+     j++
+  }
+// let p = sdt.personnes.length - 1;
+// setInterval(() => {
+//     console.log('MIAAAAOUW')
+//     if(p == 1) {
+//         clearInterval();
+//     }
 
-if (Patient.argent >= prixMedoc) {
-    // le patient paye la pharmacie
-    Patient.paye(this, prixMedoc);
-    // le patient est en bonne santé
-    Patient.etatSante = "bonne santé";
-} else {// sinon le patient devient mourrant
-    Patient.etatSante = "mourrant";
-}
+// }, 2000)
 
+  for (let i = sdt.personnes.length - 1; i >= 0; i--) {
+
+      doSetTimeout(j++);
+    //   p--;
+      
+      
+  }
 
 
 
